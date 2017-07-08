9dc4c401cd502e158fa82994a8bf1b1c3583ec5b
xinlei zhao <zxlwbi199101@gmail.com>
2017-07-08 08:05:16 +0800

    update 226 title

--- 226_Invert_Binary_Tree.cpp
+++ 226_Invert_Binary_Tree.cpp
@@ -1,9 +1,7 @@
 /*
- * https://leetcode.com/problems/maximum-depth-of-binary-tree/#/description
+ * https://leetcode.com/problems/invert-binary-tree/#/description
  *
- * Given a binary tree, find its maximum depth.
- * The maximum depth is the number of nodes along the longest path from
- * the root node down to the farthest leaf node.
+ * Invert a binary tree.
  *
  */
 
