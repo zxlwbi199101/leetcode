c027daebc3d50c070114c4465a98337c7955840b
xinlei zhao <zxlwbi199101@gmail.com>
2017-08-09 10:24:57 +0800

    88 fix

--- 88_Merge_Sorted_Array.cpp
+++ 88_Merge_Sorted_Array.cpp
@@ -14,7 +14,7 @@ class Solution {
 public:
   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     for (int i = m-- + n-- - 1; i >= 0; i--) {
-      if (nums1[m] > nums2[n]) nums1[i] = nums1[m--];
+      if (n < 0 || m >= 0 && nums1[m] > nums2[n]) nums1[i] = nums1[m--];
       else nums1[i] = nums2[n--];
     }
   }
