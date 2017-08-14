bd1aa908e5f5930a931d35290f67253b9cac75cf
xinlei zhao <zxlwbi199101@gmail.com>
2017-08-14 15:52:21 +0800

    153 binary

--- 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -19,18 +19,40 @@ using namespace std;
 // solution 1
 class Solution {
 public:
+  // int findMin(vector<int>& nums) {
+  //   if (!nums.size()) return 0;
+
+  //   int last = nums[0];
+
+  //   for (int n : nums) {
+  //     if (last > n) {
+  //       return n;
+  //     }
+  //   }
+
+  //   return last;
+  // }
+
   int findMin(vector<int>& nums) {
     if (!nums.size()) return 0;
 
-    int last = nums[0];
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid = 0;
 
-    for (int n : nums) {
-      if (last > n) {
-        return n;
+    while (low < high - 1) {
+      mid = low + (high - low) / 2;
+
+      cout << low << " " << mid << " " << high << endl;
+
+      if (nums[low] > nums[mid] || (nums[low] < nums[mid] && nums[low] < nums[high])) {
+        high = mid;
+      } else {
+        low = mid;
       }
     }
 
-    return last;
+    return nums[low] < nums[high] ? nums[low] : nums[high];
   }
 };
 
@@ -38,7 +60,9 @@ public:
 int main() {
   Solution solution;
 
-  vector<int> nums = {0,1,2,4,5,6,7};
+  vector<int> nums = {6,7,0,1,2,4,5};
+  // vector<int> nums = {4,5,6,7,0,1,2};
+  // vector<int> nums = {0,1,2,4,5,6,7};
   cout << solution.findMin(nums) << endl;
 
   return 0;
