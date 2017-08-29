caf1a9a216b513caf303ec3e8ff6112770370c5a
xinlei zhao <zxlwbi199101@gmail.com>
2017-08-30 02:13:50 +0800

    48

--- 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
+++ 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stack>
+#include <map>
 #include <queue>
 using namespace std;
 
@@ -24,12 +24,16 @@ struct TreeNode {
 class Solution {
 public:
   TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-    map<int, int> posMap;
-    for (int i = 0; i < inorder.size(); i++) {
-      posMap[n] = i;
+    if (inorder.size() == 0) {
+      return NULL;
     }
 
-    return parse(inorder, postorder, 0, inorder.size() - 1, 0, postorder,
+    map<int, int> posMap;
+    for (int i = 0; i < inorder.size(); i++) {
+      posMap[inorder[i]] = i;
+    }
+
+    return parse(inorder, postorder, 0, inorder.size() - 1, 0,
                  postorder.size() - 1, posMap);
   }
 
@@ -39,18 +43,18 @@ public:
                   int inTo,
                   int postFrom,
                   int postTo,
-                  map<int, int> pos) {
+                  map<int, int>& pos) {
     int rootVal = postorder[postTo];
     TreeNode* root = new TreeNode(rootVal);
 
     int rootPos = pos[rootVal];
     if (inFrom < rootPos) {
-      root.left = parse(inorder, postorder, inFrom, rootPos - 1, postFrom,
-                        postFrom + rootPos - inFrom, pos);
+      root->left = parse(inorder, postorder, inFrom, rootPos - 1, postFrom,
+                         postFrom + rootPos - inFrom - 1, pos);
     }
     if (inTo > rootPos) {
-      root.left = parse(inorder, postorder, rootPos + 1, inTo,
-                        postTo - rootPos + inTo, postTo, pos);
+      root->right = parse(inorder, postorder, rootPos + 1, inTo,
+                          postTo - inTo + rootPos, postTo - 1, pos);
     }
 
     return root;
@@ -64,7 +68,9 @@ int main() {
   vector<int> nums1 = {11,4,5,13,8,9};
   vector<int> nums2 = {11,4,13,9,8,5};
 
-  cout << solution.buildTree(nums1, nums2) << endl;
+  TreeNode* result = solution.buildTree(nums1, nums2);
+
+  cout << result->val << endl;
 
   return 0;
 }
---
+++ 48_Rotate_Image.cpp
@@ -0,0 +1,50 @@
+/*
+ * https://leetcode.com/problems/rotate-image/description/
+ *
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+using namespace std;
+
+// solution 1
+class Solution {
+public:
+  void rotate(vector<vector<int>>& matrix) {
+    int width = matrix.size();
+    int temp = 0;
+
+    for (int i = 0; i < (width + 1) / 2; i++) {
+      for (int j = 0; j < width / 2; j++) {
+        temp = matrix[i][j];
+
+        matrix[i][j] = matrix[width - j - 1][i]; // left top
+        matrix[width - j - i][i] = matrix[width - i - 1][width - j - 1]; // left bottom
+        matrix[width - i - 1][width - j - 1] = matrix[j][width - i - 1]; // right bottom
+        matrix[j][width - i - 1] = temp; // right top
+      }
+    }
+  }
+};
+
+// test
+int main() {
+  Solution solution;
+
+  vector<int> nums1 = {1,2,3};
+  vector<int> nums2 = {4,5,6};
+  vector<int> nums3 = {7,8,9};
+  vector<vector<int>> matrix = {nums1, nums2, nums3};
+
+  solution.rotate(matrix);
+  cout << matrix[0][0] << endl;
+
+  return 0;
+}
+
+
