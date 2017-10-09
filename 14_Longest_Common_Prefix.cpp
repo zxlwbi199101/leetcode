543e8c9805885985c25678bd7cb249baa9bdc784
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-09 21:43:02 +0800

    14

--- 14_Longest_Common_Prefix.cpp
+++ 14_Longest_Common_Prefix.cpp
@@ -18,34 +18,51 @@ using namespace std;
 // solution 1
 class Solution {
 public:
+  // string longestCommonPrefix(vector<string>& strs) {
+  //   if (!strs.size()) return "";
+  //   string result = "";
+
+  //   for (int i = 0; ; i++) {
+  //     char c = '\0';
+
+  //     for (int j = 0; j < strs.size(); j++) {
+  //       if (strs[j].size() <= i) return result;
+
+  //       if (!j) c = strs[j][i];
+  //       else if (c != strs[j][i]) return result;
+  //     }
+
+  //     result += c;
+  //   }
+
+  //   return result;
+  // }
+
   string longestCommonPrefix(vector<string>& strs) {
-    string result = "";
+    if (!strs.size()) return "";
 
-    for (int i = 0; ; i++) {
-      char c = '\0';
-
-      for (int j = 0; j < strs.size(); j++) {
-        if (strs[j].size() <= i) return result;
-
-        if (!j) c = strs[j][i];
-        else if (c != strs[j][i]) return result;
+    for (int i = 0; i < strs[0].size(); i++) {
+      for (int j = 1; j < strs.size(); j++) {
+        if (strs[j].size() <= i || strs[j][i] != strs[0][i])
+          return strs[0].substr(0, i);
       }
-
-      result += c;
     }
 
-    return result;
+    return strs[0];
   }
 };
 
 // test
 int main() {
   Solution solution;
-  cout << solution.longestCommonPrefix(vector<string> {
+
+  vector<string> strs = {
     "asdf",
     "asdfa",
     "asdfbb"
-  }) << endl;
+  };
+
+  cout << solution.longestCommonPrefix(strs) << endl;
 
   return 0;
 }
