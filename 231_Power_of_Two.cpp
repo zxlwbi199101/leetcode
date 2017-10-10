61422ccf00d9f34009d32c577b7aca19fd6fc33d
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-10 17:49:43 +0800

    231

---
+++ 231_Power_of_Two.cpp
@@ -0,0 +1,47 @@
+/*
+ * https://leetcode.com/problems/power-of-two/description/
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <ctype.h>
+using namespace std;
+
+// solution 1
+class Solution {
+public:
+  // bool isPowerOfTwo(int n) {
+  //   for (int i = 1; i <= n; i <<= 1) {
+  //     if (i == n) return true;
+  //     if (i > INT_MAX >> 1) return false;
+  //   }
+  //   return false;
+  // }
+  bool isPowerOfTwo(int n) {
+    while (n > 1) {
+      if (n % 2) return false;
+      if (n == 2) return true;
+      n >>= 1;
+    }
+    return false;
+  }
+};
+
+// test
+int main() {
+  Solution solution;
+  cout << solution.isPowerOfTwo(1073741825) << endl;
+
+  return 0;
+}
+
+
+
