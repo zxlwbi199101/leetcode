27666a758d25e09a464315c0fc7274fbe76f507c
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-12 02:46:10 +0800

    263

---
+++ 263_Ugly_Number.cpp
@@ -0,0 +1,48 @@
+/*
+ * https://leetcode.com/problems/ugly-number/description/
+ *
+ * Write a program to check whether a given number is an ugly number.
+ *
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ * For example, 6, 8 are ugly while 14 is not ugly since it includes another
+ * prime factor 7.
+ *
+ * Note that 1 is typically treated as an ugly number.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+#include <math.h>
+#include <ctype.h>
+using namespace std;
+
+// solution 1
+class Solution {
+public:
+  bool isUgly(int num) {
+    if (num < 1) return false;
+
+    while (num % 2 == 0) num /= 2;
+    while (num % 3 == 0) num /= 3;
+    while (num % 5 == 0) num /= 5;
+
+    return num == 2 || num == 3 || num == 5 || num == 1;
+  }
+};
+
+// test
+int main() {
+  Solution solution;
+  cout << solution.isUgly(58) << endl;
+
+  return 0;
+}
+
+
+
