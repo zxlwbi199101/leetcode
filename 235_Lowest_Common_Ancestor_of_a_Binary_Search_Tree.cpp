97e1b4e252349e72777d4cff7c8c8f0e47cf1782
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-14 21:30:22 +0800

    235

---
+++ 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -0,0 +1,76 @@
+/*
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of
+ * two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes v and w as the lowest node in T that has both v
+ * and w as descendants (where we allow a node to be a descendant of itself).”
+ *
+ *         _______6______
+ *        /              \
+ *     ___2__          ___8__
+ *    /      \        /      \
+ *    0      _4       7       9
+ *          /  \
+ *          3   5
+ * For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another
+ * example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of
+ * itself according to the LCA definition.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// solution 1
+class Solution {
+public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    while ((p->val > root->val && q->val > root->val) ||
+      (p->val < root->val && q->val < root->val)) {
+      root = (p->val > root->val) ? root->right : root->left;
+    }
+    return root;
+  }
+};
+
+
+// test
+int main() {
+  Solution solution;
+
+  // TreeNode l_1 = {3};
+  // TreeNode l_2 = {0};
+  // TreeNode l_3 = {4};
+  // TreeNode l_4 = {2};
+  // TreeNode l_5 = {4};
+  // TreeNode l_6 = {1};
+
+  // l_2.left = &l_4;
+  // l_2.right = &l_5;
+  // l_3.right = &l_6;
+  // l_1.left = &l_2;
+  // l_1.right = &l_3;
+
+  // cout << solution.longestUnivaluePath(&l_1) << endl;
+
+  return 0;
+}
+
+
