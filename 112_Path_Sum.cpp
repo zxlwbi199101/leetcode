eef41fa896c705fccffcf739b565e5f119a35a92
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-15 03:06:02 +0800

    112

---
+++ 112_Path_Sum.cpp
@@ -0,0 +1,78 @@
+/*
+ * https://leetcode.com/problems/path-sum/description/
+ *
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path
+ * such that adding up all the values along the path equals the given sum.
+ *
+ * For example:
+ * Given the below binary tree and sum = 22,
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// solution 1
+class Solution {
+public:
+  bool hasPathSum(TreeNode* root, int sum) {
+    if (!root) return false;
+    return check(root, sum, 0);
+  }
+
+  bool check(TreeNode* node, int sum, int cur) {
+    cur += node->val;
+    if (!node->left && !node->right) return sum == cur;
+
+    return (node->left && check(node->left, sum, cur)) ||
+      (node->right && check(node->right, sum, cur));
+  }
+};
+
+
+// test
+int main() {
+  Solution solution;
+
+  // TreeNode l_1 = {3};
+  // TreeNode l_2 = {0};
+  // TreeNode l_3 = {4};
+  // TreeNode l_4 = {2};
+  // TreeNode l_5 = {4};
+  // TreeNode l_6 = {1};
+
+  // l_2.left = &l_4;
+  // l_2.right = &l_5;
+  // l_3.right = &l_6;
+  // l_1.left = &l_2;
+  // l_1.right = &l_3;
+
+  // cout << solution.longestUnivaluePath(&l_1) << endl;
+
+  return 0;
+}
+
+
