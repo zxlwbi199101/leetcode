f6817e39d1a4c70336e73d2779cbefe1a180d95d
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-16 00:11:36 +0800

    405

---
+++ 405_Convert_a_Number_to_Hexadecimal.cpp
@@ -0,0 +1,70 @@
+/*
+ * https://leetcode.com/problems/convert-a-number-to-hexadecimal/description/
+ *
+ * Given an integer, write an algorithm to convert it to hexadecimal. For
+ * negative integer, two’s complement method is used.
+ *
+ * Note:
+ *
+ * All letters in hexadecimal (a-f) must be in lowercase.
+ * The hexadecimal string must not contain extra leading 0s. If the number is
+ * zero, it is represented by a single zero character '0'; otherwise, the first
+ * character in the hexadecimal string will not be the zero character.
+ * The given number is guaranteed to fit within the range of a 32-bit signed
+ * integer.
+ * You must not use any method provided by the library which converts/formats
+ * the number to hex directly.
+ * Example 1:
+ *
+ * Input:
+ * 26
+ *
+ * Output:
+ * "1a"
+ * Example 2:
+ *
+ * Input:
+ * -1
+ *
+ * Output:
+ * "ffffffff"
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// solution 1
+class Solution {
+public:
+  string toHex(int num) {
+    if (num == 0) return "0";
+
+    string result = "";
+    char MAP[] = {'0', '1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+
+    while (num != 0) {
+      result = MAP[(num & 15)] + result;
+      num = (unsigned)num >> 4;
+    }
+
+    return result;
+  }
+};
+
+// test
+int main() {
+  Solution solution;
+
+  cout << solution.toHex(-1) << endl;
+
+  return 0;
+}
+
+
+
