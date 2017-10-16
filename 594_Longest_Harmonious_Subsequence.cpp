e0f326b774137fcf0c413d67916621547732c1fd
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-16 10:08:44 +0800

    594

---
+++ 594_Longest_Harmonious_Subsequence.cpp
@@ -0,0 +1,55 @@
+/*
+ * https://leetcode.com/problems/longest-harmonious-subsequence/description/
+ *
+ * We define a harmonious array is an array where the difference between its
+ * maximum value and its minimum value is exactly 1.
+ *
+ * Now, given an integer array, you need to find the length of its longest
+ * harmonious subsequence among all its possible subsequences.
+ *
+ * Example 1:
+ * Input: [1,3,2,2,5,2,3,7]
+ * Output: 5
+ * Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+ * Note: The length of the input array will not exceed 20,000.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+using namespace std;
+
+// solution 1
+class Solution {
+public:
+  int findLHS(vector<int>& nums) {
+    unordered_map<int, int> m;
+    int result = 0;
+
+    for (int n : nums) m[n]++;
+    for (auto it = m.begin(); it != m.end(); it++) {
+      if (m.find(it->first + 1) != m.end()) {
+        result = max(result, it->second + m[it->first + 1]);
+      }
+    }
+
+    return result;
+  }
+};
+
+// test
+int main() {
+  Solution solution;
+
+  vector<int> nums = {1,3,2,2,5,2,3,7};
+  cout << solution.findLHS(nums) << endl;
+
+  return 0;
+}
+
+
+
