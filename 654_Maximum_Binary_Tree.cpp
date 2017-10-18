f3dfab3ac99d3ee55b88061017d167cf8ccec942
xinlei zhao <zxlwbi199101@gmail.com>
2017-10-18 09:26:22 +0800

    654

---
+++ 654_Maximum_Binary_Tree.cpp
@@ -0,0 +1,103 @@
+/*
+ * https://leetcode.com/problems/maximum-binary-tree/description/
+ *
+ * Given an integer array with no duplicates. A maximum tree building on this
+ * array is defined as follow:
+ *
+ * The root is the maximum number in the array.
+ * The left subtree is the maximum tree constructed from left part subarray
+ * divided by the maximum number.
+ * The right subtree is the maximum tree constructed from right part subarray
+ * divided by the maximum number.
+ * Construct the maximum tree by the given array and output the root node of
+ * this tree.
+ *
+ * Example 1:
+ * Input: [3,2,1,6,0,5]
+ * Output: return the tree root node representing the following tree:
+ *
+ *       6
+ *     /   \
+ *    3     5
+ *     \    /
+ *      2  0
+ *        \
+ *         1
+ * Note:
+ * The size of the given array will be in the range [1,1000].
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <set>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+2 3 1 4 2
+// solution 1
+class Solution {
+public:
+  TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+    stack<TreeNode*> stk;
+    int m = INT_MIN;
+    TreeNode* maxNode = NULL;
+
+    for (int i = 0; i < nums.size(); i++) {
+      TreeNode* node = new TreeNode(nums[i]);
+      if (m < nums[i]) {
+        maxNode = node;
+        m = nums[i];
+      }
+
+      if (i == 0) {
+        stk.push(node);
+        continue;
+      }
+
+      if (i > 0 && nums[i] < nums[i - 1]) {
+        stk.top()->right = node;
+        stk.push(node);
+      } else {
+        TreeNode* bigger = NULL;
+        TreeNode* last = NULL;
+
+        while (!stk.empty()) {
+          if (stk.top()->val > nums[i]) {
+            bigger = stk.top();
+            break;
+          }
+          last = stk.top();
+          stk.pop();
+        }
+
+        node->left = last;
+        if (bigger) bigger->right = node;
+        stk.push(node);
+      }
+    }
+
+    return maxNode;
+  }
+
+};
+
+
+// test
+int main() {
+  Solution solution;
+  return 0;
+}
+
+
